8f7ffb5f9e319ad55bdab4b2a9cc10cd91f7d654
Max Hipperson <40496579+maxhipperson@users.noreply.github.com>
2023-05-26 19:51:28 +0100

    refactor: stop checking and installing requirements when using hub

--- hubconf.py
+++ hubconf.py
@@ -10,12 +10,11 @@ from pathlib import Path
 import torch
 
 from models.yolo import Model
-from utils.general import check_requirements, set_logging
+from utils.general import set_logging
 from utils.google_utils import attempt_download
 from utils.torch_utils import select_device
 
 dependencies = ['torch', 'yaml']
-check_requirements(Path(__file__).parent / 'requirements.txt', exclude=('pycocotools', 'thop'))
 set_logging()
 
 
